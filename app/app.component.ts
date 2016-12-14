d4bb297540f50ff5e2b634f3fdc766497b7b4a52
tomvanboghout <tvboghout@gmail.com>
2016-12-14 09:54:39 +0100

    Initial setup

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<my-heroes></my-heroes>`,
 })
 export class AppComponent  { name = 'Angular'; }
